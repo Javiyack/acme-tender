ae196a173a2ecf0d83a182fb3d8687312812cdfe
frallafer <frallafer@222a3260-a37a-4893-ab48-06aaebc13f27>
2018-06-07 14:40:49 +0000

    Colaboration resquest improvement

--- src/main/java/repositories/OfferRepository.java
+++ src/main/java/repositories/OfferRepository.java
@@ -35,9 +35,23 @@ public interface OfferRepository extends JpaRepository<Offer, Integer> {
 	@Query("select o from Offer o where o.published = false ")
 	Collection<Offer> findAllNotPublished();
 
-	@Query("select o from Offer o " + " where o.published = true " + " and (" + "        (o.id in (select ss.offer.id from SubSection ss where (ss.title like %?1% or ss.shortDescription like %?1% or ss.body like %?1% or ss.comments like %?1%))) "
-		+ "     or (o.tender.title like %?1% or o.tender.expedient like %?1% or o.tender.organism like %?1% or o.tender.bulletin like %?1% or o.tender.observations like %?1% or o.tender.informationPage like %?1% or o.tender.interestComment like %?1%) "
-		+ "     )")
+	@Query("select o "
+			+ "from Offer o "
+			+ "where (o.id in ("
+				+ "select ss.offer.id "
+				+ "from SubSection ss "
+				+ "where ss.title like %?1% "
+				+ "or ss.shortDescription like %?1% "
+				+ "or ss.body like %?1% "
+				+ "or ss.comments like %?1%) "
+			+ "or o.tender.title like %?1% "
+			+ "or o.tender.expedient like %?1% "
+			+ "or o.tender.organism like %?1% "
+			+ "or o.tender.bulletin like %?1% "
+			+ "or o.tender.observations like %?1% "
+			+ "or o.tender.informationPage like %?1% "
+			+ "or o.tender.interestComment like %?1%) "
+			+ "and o.published = true")
 	Collection<Offer> findOfferByKeyword(String word);
 
 	@Query("select o from Offer o where o.tender.id = ?1")
